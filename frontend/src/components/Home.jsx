990d70ed6ce659595ddd629d9a43f0ecfdafa454
albertoornelasdev <albertoornelasdev@gmail.com>
2022-03-06 01:13:38 -0700

    display user data

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react"
-import { Flex } from "@chakra-ui/react"
+import { Flex, Text, Button, Link, Heading, Box } from "@chakra-ui/react"
 import axios from "axios"
+import { Link as LinkRouter } from 'react-router-dom'
 
 const Home = () => {
-  const [auth, setAuth] = useState(true)
+  const [auth, setAuth] = useState(false)
+  const [userId, setUserId] = useState('')
+  const [currEmail, setCurrEmail] = useState('')
 
   // Verify token
   useEffect(() => {
@@ -13,17 +16,51 @@ const Home = () => {
       }
     }).then((res) => {
       console.log(res)
-      if (res.status === 200) setAuth(true)
+      if (res.status === 200) {
+        setAuth(true)
+        setUserId(res.data.id)
+      } else {
+        setAuth(false)
+        setUserId('')
+      }
     }).catch((error) => {
       console.log(error)
     })
   }, [])
 
+  // Get User info
+  useEffect(() => {
+    axios.get(process.env.REACT_APP_GET_USER_URL + userId)
+      .then((res) => {
+        console.log(res)
+        setUserId(res.data.id)
+        setCurrEmail(res.data.email)
+      }).catch((error) => {
+        console.log(error)
+      })
+  })
+
   return (
-    <Flex>
-      Home
-      {auth ? "You are logge in" : "Not loading"}
-    </Flex>
+    <Flex minHeight='90vh' width='full' alignItems='center' justifyContent='center' >
+      {!auth && (
+        <Flex flexDir="column" alignItems="center">
+          <Text color={"tomato"}>You are not logged In</Text>
+          <Link as={LinkRouter} color="green.500" to="/login">Go to Login</Link>
+        </Flex>
+      )}
+      {auth &&
+        <Flex flexDir="column" alignItems="center">
+          <Heading size={"lg"}>Welcome</Heading>
+          <Box p={5}>
+            <Text>ID: {userId}</Text>
+            <Text>Email: {currEmail}</Text>
+          </Box>
+          <Button bg={"tomato"} color={"white"}>
+            Log out
+          </Button>
+        </Flex>
+      }
+    </Flex >
   )
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Flex, Box, Heading, Spacer, Button } from '@chakra-ui/react'
-
+import { Flex, Box, Heading } from '@chakra-ui/react'
 
 const Navbar = () => {
   return (
@@ -10,13 +9,6 @@ const Navbar = () => {
       boxShadow="lg">
       <Box p='4' color="gray.600">
         <Heading size='md'>Login System App</Heading>
-      </Box>
-      <Spacer />
-      <Box p='4'>
-        <Button colorScheme='teal' mr='4' variant="outline">
-          Sign Up
-        </Button>
-        <Button colorScheme='teal'>Log in</Button>
       </Box>
     </Flex>
   )
